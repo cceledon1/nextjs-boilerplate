2b8d95bd9223b6566bbfb3af60e91c6de74e3b89
cceledon1 <34894177+cceledon1@users.noreply.github.com>
2025-08-07 16:06:04 +0300

    Add .next/static/chunks/src_app_contacto_page_tsx_45831eb8._.js 1754571964154

---
+++ .next/static/chunks/src_app_contacto_page_tsx_45831eb8._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_1ef7e78f._.js",
+  "static/chunks/src_9beb47ca._.js"
+],
+    source: "dynamic"
+});
