327a13ae7a2d4e281c86001b371914f5ab8fddc7
cceledon1 <34894177+cceledon1@users.noreply.github.com>
2025-08-07 16:06:03 +0300

    Add .next/static/chunks/src_app_emergencia_page_tsx_45831eb8._.js 1754571962436

---
+++ .next/static/chunks/src_app_emergencia_page_tsx_45831eb8._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/_a7c1f322._.js"
+],
+    source: "dynamic"
+});
